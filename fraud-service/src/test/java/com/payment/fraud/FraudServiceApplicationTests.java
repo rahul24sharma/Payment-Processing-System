c8c4be6355e7e116f66031ff8b4ef454543bed50
rahul24sharma <sharma.rahul1@northeastern.edu>
2026-02-04 01:01:11 -0500

    folders structures setup complete

---
+++ fraud-service/src/test/java/com/payment/fraud/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
